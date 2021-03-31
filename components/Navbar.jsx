538399a73402772496e81cf3d27687f5fdc7c37a
Alexandra Fazakas <alexandra.fazakas91@gmail.com>
2021-03-31 19:16:26 +0300

    adds landing page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Navbar() {
     return (<>
         <nav className="main-nav">
-            <img className="logo" src="/assets/navbar/logo.jpg" alt="logo" />
+            <Link href="/"><img className="logo" src="/assets/navbar/logo.jpg" alt="logo" /></Link>
             <ul className="list">
                 <Link href="/preturi"><a>Preturi</a></Link>
                 <Link href="/livrare"><a>Livrare</a></Link>
---
+++ components/OrderStepCard.jsx
@@ -0,0 +1,14 @@
+export default function OrderStepCard(props) {
+    return (<div className="order-step-card">
+        <div className="header">
+            <div className="number-wraper">
+                <span className="number selected">{props.number}</span>
+            </div>
+            <h1>{props.title}</h1>
+        </div>
+        <div className="text-container">
+            <div>{props.text}</div>
+        </div>
+    </div>
+    )
+}

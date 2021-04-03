301175fc88ae485bbfb86c2c654d6d0e5cbc44c1
Charles <samannaphala@gmail.com>
2021-04-03 11:19:02 +0300

    removes unnecessary imports and props

--- pages/preturi.jsx
+++ pages/preturi.jsx
@@ -4,28 +4,27 @@ import EnvelopeCard from "../components/EnvelopeCard";
 import SealCard from '../components/SealCard'
 import PricesTotal from "../components/PricesTotal";
 
-const optionsComponents = [EnvelopeCard, SealCard]
-
 export default function Preturi() {
     const [selectedItems, setSelectedItems] = useState([])
-    const [totalPrice, setTotalPrice] = useState(0)
-    const CurrentComponent = optionsComponents[0]
     const [steps, setSteps] = useState([
-        { id: 1, product: 'envelopes', currentSelection: true },
-        { id: 2, product: 'paper', currentSelection: false },
-        { id: 3, product: 'menus', currentSelection: false },
-        { id: 4, product: 'addons', currentSelection: false }
+        { id: 1, product: 'envelopes', currentSelection: true, component: EnvelopeCard },
+        { id: 2, product: 'paper', currentSelection: false, component: 'PaperCard' },
+        { id: 3, product: 'menus', currentSelection: false, component: 'MenuCard' },
+        { id: 4, product: 'seals', currentSelection: false, component: SealCard },
+        { id: 4, product: 'addons', currentSelection: false, component: 'AddonsCard' }
     ])
+
+    const CurrentComponent = steps[0].component
 
     return (<div className="page preturi row">
         <div className="navsteps-component-wraper">
             <NavSteps steps={steps} setSteps={setSteps}/>
         <div className="step-component">
-            <CurrentComponent selectedItems={selectedItems} setSelectedItems={setSelectedItems} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
+            <CurrentComponent selectedItems={selectedItems} setSelectedItems={setSelectedItems} />
         </div>
         </div>
         <div className="price-calculator">
-            <PricesTotal selectedItems={selectedItems} totalPrice={totalPrice} setTotalPrice={setTotalPrice} />
+            <PricesTotal selectedItems={selectedItems} />
         </div>
     </div>
     )

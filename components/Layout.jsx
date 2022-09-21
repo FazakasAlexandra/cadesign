1b697e3a91b15d39dc338ffedf9894b30baec801
Charles Ko <samannaphala@gmail.com>
2022-09-21 11:59:57 +0300

    change

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout(props) {
     return (
         <>
             <Head>
-                <title>Invitatii handmade pentru nunta</title>
+                <title>Invitatii handmade</title>
                 <meta charSet="UTF-8" />
                 <meta name="keywords" content="invitatii pentru nunta, hartie handmade, arta manual, plicuri, siginii, nunta, invitatie, special"></meta>
                 <meta name="description" content="Confectionam invitatii personalizate pentru nunta lucrate manual, de cea mai buna calitate si la cel mai bun pret"></meta>
